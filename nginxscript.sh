a429ee8a379fe70da083e4c121eaffb94c2178b2
fpk2014 <994347033@qq.com>
2016-04-27 02:32:32 +0800

    Update nginxscript.sh

--- nginxscript.sh
+++ nginxscript.sh
@@ -14,5 +14,5 @@ tar -zxvf nginx_mod_h264_streaming-2.2.7.tar.gz
 unzip -zxvf nginx-rtmp-module-master.zip
 tar -zxvf nginx-1.8.1.tar.gz
 cd nginx-1.8.1
-./configure --user=www --group=www --prefix=/usr/local/nginx --with-http_stub_status_module --with-http_ssl_module --with-http_spdy_module --with-http_gzip_static_module --with-ipv6 --with-http_sub_module --with-http_flv_module --with-http_mp4_module --with-pcre=/root/nginxscript/pcre-7.9 --with-zlib=/root/nginxscript/zlib-1.2.3 --add-module=/root/nginxscript/nginx_mod_h264_streaming-2.2.7 --with-http_mp4_module --with-debug --add-module=/root/nginxscript/nginx-rtmp-module-master
+./configure --user=www --group=www --prefix=/usr/local/nginx --with-http_stub_status_module --with-http_ssl_module --with-http_spdy_module --with-http_gzip_static_module --with-ipv6 --with-http_sub_module --with-http_flv_module --with-http_mp4_module --with-pcre=/root/Building-MP4-flv-streaming-media-server/pcre-7.9 --with-zlib=/root/Building-MP4-flv-streaming-media-server/zlib-1.2.3 --add-module=/root/Building-MP4-flv-streaming-media-server/nginx_mod_h264_streaming-2.2.7 --with-http_mp4_module --with-debug --add-module=/root/Building-MP4-flv-streaming-media-server/nginx-rtmp-module-master
 make && make install
